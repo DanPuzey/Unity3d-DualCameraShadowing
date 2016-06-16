2ba30391174ce8db23c4f002889da791aaa5b74e
Dan Puzey <commit@puzey.net>
2016-06-16 17:01:57 +0100

    Capture a texture from one camera and pass it through to subsequent cameras.

--- Assets/MultiCameraRenderer.cs
+++ Assets/MultiCameraRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Camera))]
 public class MultiCameraRenderer : MonoBehaviour
@@ -7,9 +8,21 @@ public class MultiCameraRenderer : MonoBehaviour
     public Camera[] SourceCameras;
     public bool OverrideRenderCameraSettings = true;
     public bool DelayCameraDeactivate = true;
+    public bool PassThroughShadowMap = true;
+
+    public Light LightSources;
+    public Shader ReplacementShader;
+
+    [Header("Output settings")]
+    public BuiltinRenderTextureType TargetTexture = BuiltinRenderTextureType.CurrentActive;
+    public CameraEvent CameraEvent = CameraEvent.AfterGBuffer;
+    public bool UpdateSettings = false;
 
     private Camera _renderCamera;
     private RenderTexture _renderTexture;
+    private CommandBuffer _copyShadowMap;
+
+    private const string GlobalShadowmapName = "_GlobalShadowMap";
 
     private void Awake()
     {
@@ -22,11 +35,19 @@ public class MultiCameraRenderer : MonoBehaviour
             _renderCamera.useOcclusionCulling = false;
             _renderCamera.hdr = false;
         }
+
+        if (PassThroughShadowMap && SourceCameras.Length > 1)
+        {
+            AddCommandBufferToSourceCamera();
+
+            SetReplacementShaderOnDestinationCameras();
+        }
     }
 
     private void Start()
     {
         StartCoroutine(DeactivateCameras());
+        InvokeRepeating("CheckForRefresh", 1f, 1f);
     }
 
     private void OnPreRender()
@@ -42,6 +63,12 @@ public class MultiCameraRenderer : MonoBehaviour
         }
     }
 
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(_renderTexture, dest);
+        RenderTexture.ReleaseTemporary(_renderTexture);
+    }
+
     private IEnumerator DeactivateCameras()
     {
         if (DelayCameraDeactivate)
@@ -55,9 +82,35 @@ public class MultiCameraRenderer : MonoBehaviour
         }
     }
 
-    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    private void AddCommandBufferToSourceCamera()
     {
-        Graphics.Blit(_renderTexture, dest);
-        RenderTexture.ReleaseTemporary(_renderTexture);
+        Debug.Log("Updating command buffer");
+
+        SourceCameras[0].RemoveAllCommandBuffers();
+
+        _copyShadowMap = new CommandBuffer();
+        _copyShadowMap.name = "Copy shadow map";
+        _copyShadowMap.SetGlobalTexture(GlobalShadowmapName, new RenderTargetIdentifier(TargetTexture));
+
+        SourceCameras[0].AddCommandBuffer(CameraEvent, _copyShadowMap);
+
+        Debug.Log("Command buffer update complete");
+    }
+
+    private void SetReplacementShaderOnDestinationCameras()
+    {
+        for (var i = 1; i < SourceCameras.Length; i++)
+        {
+            SourceCameras[i].SetReplacementShader(ReplacementShader, null);
+        }
+    }
+
+    private void CheckForRefresh()
+    {
+        if (UpdateSettings)
+        {
+            UpdateSettings = false;
+            AddCommandBufferToSourceCamera();
+        }
     }
 }
---
+++ Assets/Shaders/UseGlobalShadowData.shader
@@ -0,0 +1,34 @@
+﻿Shader "Custom/UseGlobalShadowData" {
+    Properties{
+        _Color("Color", Color) = (1,1,1,1)
+    }
+    SubShader{
+        Tags{ "RenderType" = "Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _GlobalShadowMap;
+
+    struct Input {
+        float2 uv_GlobalShadowMap;
+    };
+
+    half _Glossiness;
+    half _Metallic;
+    fixed4 _Color;
+
+    void surf(Input IN, inout SurfaceOutputStandard o) {
+        // Albedo comes from a texture tinted by color
+        fixed4 c = tex2D(_GlobalShadowMap, IN.uv_GlobalShadowMap) * _Color;
+        o.Albedo = c.rgb;
+    }
+    ENDCG
+    }
+        FallBack "Diffuse"
+}
