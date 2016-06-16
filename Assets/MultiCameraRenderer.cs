637373e3df1ad67a489ab928aa8531a0c2810fd6
Dan Puzey <commit@puzey.net>
2016-06-16 11:13:48 +0100

    Multi-camera setup with deferred rendering, HDR, and composite camera component.

---
+++ Assets/MultiCameraRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultiCameraRenderer : MonoBehaviour
+{
+    public Camera[] SourceCameras;
+    public bool OverrideRenderCameraSettings = true;
+    public bool DelayCameraDeactivate = true;
+
+    private Camera _renderCamera;
+    private RenderTexture _renderTexture;
+
+    private void Awake()
+    {
+        _renderCamera = GetComponent<Camera>();
+
+        if (OverrideRenderCameraSettings)
+        {
+            _renderCamera.cullingMask = 0;
+            _renderCamera.clearFlags = CameraClearFlags.Nothing;
+            _renderCamera.useOcclusionCulling = false;
+            _renderCamera.hdr = false;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DeactivateCameras());
+    }
+
+    private void OnPreRender()
+    {
+        _renderTexture = RenderTexture.GetTemporary(_renderCamera.pixelWidth, _renderCamera.pixelHeight, 24, RenderTextureFormat.ARGBFloat);
+
+        for (var i = 0; i < SourceCameras.Length; i++)
+        {
+            var c = SourceCameras[i];
+            c.targetTexture = _renderTexture;
+            c.Render();
+            c.targetTexture = null;
+        }
+    }
+
+    private IEnumerator DeactivateCameras()
+    {
+        if (DelayCameraDeactivate)
+        {
+            yield return null;
+        }
+
+        for (var i = 0; i < SourceCameras.Length; i++)
+        {
+            SourceCameras[i].enabled = false;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(_renderTexture, dest);
+        RenderTexture.ReleaseTemporary(_renderTexture);
+    }
+}
